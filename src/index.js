d7930924158e3412b6425c72c7bb271e4480b1c3
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-16 23:20:32 -0500

    initial commit with firebase config

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+
+// Firebase database config
+const firebaseConfig = {
+  apiKey: 'AIzaSyBmIzrBsw2_xkfKHyBi-NnMsgHlbnlL9kw',
+  authDomain: 'mini-url-d15ac.firebaseapp.com',
+  projectId: 'mini-url-d15ac',
+  storageBucket: 'mini-url-d15ac.appspot.com',
+  messagingSenderId: '581436341123',
+  appId: '1:581436341123:web:8ecaa560d616847290d4b6',
+  measurementId: 'G-Q594VB2WJY',
+};
+initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
