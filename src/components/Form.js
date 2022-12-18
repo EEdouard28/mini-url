31d364587471e1e2f21f7efc297e013f6ed1954f
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-17 22:35:09 -0500

    form component url function added

---
+++ src/components/Form.js
@@ -0,0 +1,53 @@
+import React from 'react';
+// generates Ids URL safe
+import { nanoid } from 'nanoid';
+// firebase database methods
+import { getDatabase, child, ref, set, get } from 'firebase/database';
+// validates web url
+import { isWebUri } from 'valid-url';
+import { OverlayTrigger } from 'react-bootstrap';
+// message
+import Tooltip from 'react-bootstrap';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // stores long URL input made by user
+      longURL: '',
+      //   stores users preferedAlias
+      preferedAlias: '',
+      //   generated URL for user auto generated or users input
+      generatedURL: '',
+      //   visual loading
+      loading: false,
+      //   keep track of fields with errors
+      errors: [],
+      errorMessage: {},
+      toolTipMessage: 'Copy To Clip Board',
+    };
+  }
+  //   When the user clicks submit, this will be called
+  onSubmit = async (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+      generatedURL: '',
+    });
+    // validate the input the user has submitted
+    var isFormValid = await this.validateInput();
+    if (!isFormValid) {
+      return;
+    }
+
+    // if the user has input a preferred alias then we use it, if not, we genertae one
+    // Be sure to change minilinkit.com to your domain
+    var generatedKey = nanoid(5);
+    var generatedURL = 'minilinkit.com/' + generatedKey;
+
+    if (this.state.preferedAlias !== '') {
+      generatedKey = this.state.preferedAlias;
+      generatedURL = 'minilinkit.com/' + this.state.preferedAlias;
+    }
+  };
+}
