45d28e2669dae59e66e0d3d5fc58b208790b893b
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-19 19:23:07 -0500

    routes created

--- server/app/main.py
+++ server/app/main.py
@@ -11,3 +11,30 @@ cred_obj = firebase_admin.get_credentials.Certificate(
 default_app = firebase_admin.initialize_app(cred_obj, {
     'databaseURL': 'https://mini-url-d15ac-default-rtdb.firebaseio.com/'
 })
+
+# Initialize flask app
+app = Flask(__name__, static_folder='./build/static',
+            template_folder='./build')
+
+
+@app.route("/")
+def hello_world():
+    return redirect("/app")
+
+
+@app.route("/app")
+def hello_world():
+    return render_template('index.html')
+
+# Method which access to the database and redirects longURL
+
+
+@app.route('/<path:generatedKey>', method=['GET'])
+def fetch_from_firebase(generatedKey):
+    ref = db.reference('/' + generatedKey)
+    data = ref.get()
+    if not data:
+        return '404 not found'
+    else:
+        longURL = data['longURL']
+        return redirect(longURL)
