7bb618b926762c79fab9c4d682b2446f896b480b
EEdouard28 <111817163+EEdouard28@users.noreply.github.com>
2022-12-19 19:45:33 -0500

    requirements added and wsgi

--- server/app/main.py
+++ server/app/main.py
@@ -1,11 +1,11 @@
 # Flask server ... Render the build on one application
 from flask import Flask, redirect, render_template
 import firebase_admin
-from firebase_admimn import db
+from firebase_admin import db
 import os
 
 # Provides python app access to firebase database
-cred_obj = firebase_admin.get_credentials.Certificate(
+cred_obj = firebase_admin.credentials.Certificate(
     './ServiceAccountKey.json')
 # Allows access to the database
 default_app = firebase_admin.initialize_app(cred_obj, {
@@ -23,13 +23,13 @@ def hello_world():
 
 
 @app.route("/app")
-def hello_world():
+def homepage():
     return render_template('index.html')
 
 # Method which access to the database and redirects longURL
 
 
-@app.route('/<path:generatedKey>', method=['GET'])
+@app.route('/<path:generatedKey>', methods=['GET'])
 def fetch_from_firebase(generatedKey):
     ref = db.reference('/' + generatedKey)
     data = ref.get()
---
+++ server/wsgi.py
@@ -0,0 +1,6 @@
+# gateway into the application
+from app.main import app
+
+# triggers application
+if __name__ == '__main__':
+    app.run()
